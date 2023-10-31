e63583595e1886af1c5a66b4a6e62a6267d188f9
taes12 <tmdxo243@gmail.com>
2023-10-31 14:11:41 +0900

    login,password test01

--- projectbook-web/src/main/java/com/projectbook/web/controller/MemberController.java
+++ projectbook-web/src/main/java/com/projectbook/web/controller/MemberController.java
@@ -13,11 +13,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.projectbook.web.domain.Member;
 import com.projectbook.web.service.MemberService;
 
 @Controller
+@SessionAttributes("member")
 public class MemberController {
 	private MemberService memberService;
 
